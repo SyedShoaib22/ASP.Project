5d85bcd0b03d7eabcda6e474790cefb825bb2213
SyedShoaib22 <139268750+SyedShoaib22@users.noreply.github.com>
2024-02-13 14:58:14 -0500

    Update InsureeController.cs

--- Controllers/InsureeController.cs
+++ Controllers/InsureeController.cs
@@ -1,47 +1,81 @@
-﻿using ASP.NET_MVC.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Linq;
 
-namespace ASP.NET_MVC.Controllers
+public class HomeController : Controller
 {
-    public class InsureeController : Controller
+    private readonly InsuranceContext _context;
+
+    public HomeController(InsuranceContext context)
     {
-        private readonly YourDbContext _dbContext;
-
-        public InsureeController(YourDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
-
-        // Other controller actions...
-
-        [HttpPost]
-        public ActionResult CalculateQuote(InsureeViewModel model)
-        {
-            decimal quote = CalculateInsuranceQuote(model); // Implement the quote calculation logic
-
-            var insuree = new Insuree
-            {
-                FirstName = model.FirstName,
-                LastName = model.LastName,
-                Email = model.Email,
-                Quote = quote
-            };
-
-            _dbContext.Insurees.Add(insuree);
-            _dbContext.SaveChanges();
-
-            return RedirectToAction("Index"); // Redirect to the appropriate view
-        }
-
-        // Other controller actions...
-
-        private decimal CalculateInsuranceQuote(InsureeViewModel model)
-        {
-            // Implement the quote calculation logic (as in the previous examples)
-            // ...
-
-            return calculatedQuote;
-        }
+        _context = context;
     }
 
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Insuree insuree)
+    {
+        if (ModelState.IsValid)
+        {
+            decimal quote = 50;
+
+            if (insuree.Age <= 18)
+                quote += 100;
+            else if (insuree.Age >= 19 && insuree.Age <= 25)
+                quote += 50;
+            else
+                quote += 25;
+
+            if (insuree.CarYear < 2000)
+                quote += 25;
+            else if (insuree.CarYear > 2015)
+                quote += 25;
+
+            if (insuree.CarMake == "Porsche")
+            {
+                quote += 25;
+
+                if (insuree.CarModel == "911 Carrera")
+                    quote += 25;
+            }
+
+            quote += insuree.SpeedingTickets * 10;
+
+            if (insuree.HasDUI)
+                quote += quote * 0.25;
+
+            if (insuree.IsFullCoverage)
+                quote += quote * 0.5;
+
+            insuree.Quote = quote;
+
+            _context.Insurees.Add(insuree);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        return View(insuree);
+    }
+
+    public IActionResult Admin()
+    {
+        var allQuotes = _context.Insurees
+            .Select(i => new { i.FirstName, i.LastName, i.Email, i.Quote })
+            .ToList();
+
+        return View(allQuotes);
+    }
 }
